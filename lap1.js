8107fa268f506f18dd706e81895958f0d7544d0b
Nguyen Quang Nang Tu <nguyentu23061981@gmail.com>
2026-09-17 15:28:48 +0700

    xong bài 2,3,4

--- lap1.js
+++ lap1.js
@@ -1,9 +1,26 @@
 console.log('day la lap 1');
+//bài 1,2
 let name = "Nguyễn Quang Năng Tú";
 let age = 20;
 const address = "Hà Nội";
 const isStudent = true;
 console.log("Họ tên:", name);
 console.log("Tuổi:", age);
+age = 21;
+console.log("Tuổi:", age);
 console.log("Địa chỉ:", address);
 console.log("Sinh viên:", isStudent);
+//bài 3
+const a=10;
+const b=5;
+console.log("Tổng :",a+b);
+console.log("Hiệu:",a-b);
+console.log("Tích:",a*b);
+console.log("Thương:",a/b);
+//bài 4 
+function sayHello(name){
+    console.log("Xin chào",name);
+}
+sayHello("An");
+sayHello("Tú");
+sayHello("Nam");
