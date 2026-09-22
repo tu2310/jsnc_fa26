9764d17e9615b3bb8051a79187593f3ebe7acdf8
Nguyen Quang Nang Tu <nguyentu23061981@gmail.com>
2026-09-22 15:22:20 +0700

    hoan thien lap 2

--- lap2.js
+++ lap2.js
@@ -53,3 +53,41 @@ console.log("Tuổi sinh viên thứ hai:", students[1].age);
 for (let i = 0; i < students.length; i++) {
     console.log("Tên sinh viên:", students[i].name);
 }
+
+//bai 5
+let html = document.getElementById("students");
+let content = "";
+for (let i = 0; i < students.length; i++) {
+   content +=
+  `<div>
+  <h3>tên sinh viên: ${students[i].name}</h3>
+  <h4>Age: ${students[i].age}</h4>
+  </div>`;
+}
+html.innerHTML = content;
+console.log(html);
+//bai tap tổng hợp
+const products = [
+  { id: 1, name: "iPhone 15", price: 20000000 },
+  { id: 2, name: "Samsung Galaxy S24", price: 5000000 },
+  { id: 3, name: "Xiaomi 14", price: 3000005 }
+];
+console.log("Danh sách sản phẩm:", products);
+let totalPrice = 0; 
+for (let i = 0; i < products.length; i++) {
+  console.log("Tên sản phẩm:", products[i].name);
+  console.log("Giá sản phẩm:", products[i].price);
+  totalPrice += products[i].price;
+}
+console.log("Tổng giá trị sản phẩm:", totalPrice);
+let htmlProducts = document.getElementById("products");
+let contents = "";
+for (let i = 0; i < products.length; i++) {
+   contents +=
+  `<div>
+  <h3>tên san pham: ${products[i].name}</h3>
+  <h4>Giá: ${products[i].price}</h4>
+  </div>`;
+}
+contents += `<h4>Tổng giá trị: ${totalPrice}</h4>`;
+htmlProducts.innerHTML = contents;
